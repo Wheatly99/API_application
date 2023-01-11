b122a4c0927a369ec1c74231145ccd30b01798e1
saneg11 <stil222@mail.ru>
2023-01-12 00:23:26 +0500

    Add test

--- test_main.py
+++ test_main.py
@@ -15,6 +15,17 @@ def test_predict():
                            json={"text": "Меня зовут Алекс и я [MASK] в Екатеринбурге."}
                            )
     json_data = response.json()
-    print(json_data)
     assert response.status_code == 200
+    assert len(json_data.keys()) == 5
     assert json_data["1"]["token_str"] == "живу"
+    assert json_data["1"]["score"] >= 0.9
+
+
+def test_predict_two():
+    response = client.post("/predict/",
+                           json={"text": "[MASK] - совокупность качеств человека, которые приобретаются им в процессе жизни в обществе."}
+                           )
+    json_data = response.json()
+    assert response.status_code == 200
+    assert len(json_data.keys()) == 5
+    assert json_data["1"]["token_str"] == "Личность"
